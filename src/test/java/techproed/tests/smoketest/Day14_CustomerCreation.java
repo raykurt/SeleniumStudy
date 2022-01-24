5e974457b5121601a7a1c88e0efb5b77e3b8a055
Recep Aykurt <recep.aykurt7@gmail.com>
2022-01-24 22:19:25 +0300

    Customer_Creation

--- src/test/java/techproed/tests/smoketest/Day14_CustomerCreation.java
+++ src/test/java/techproed/tests/smoketest/Day14_CustomerCreation.java
@@ -1,6 +1,5 @@
 package techproed.tests.smoketest;
 
-import com.github.javafaker.Faker;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import techproed.pages.CreateCustomerPage;
@@ -13,47 +12,49 @@ import techproed.utilities.ReusableMethods;
 import java.io.IOException;
 
 public class Day14_CustomerCreation {
+    LoginPage loginPage = new LoginPage();
     EmployeeDefaultPage employeeDefaultPage = new EmployeeDefaultPage();
     CreateCustomerPage createCustomerPage = new CreateCustomerPage();
-    LoginPage loginPage = new LoginPage();
-
     @Test
-    public void createCustomerAsEmployee() throws InterruptedException, IOException {
+    public void createCustomerAsEmployee() throws IOException {
         Driver.getDriver().get(ConfigurationReader.getProperty("gmi_login_url"));
+//        calling the loginApplication method to log in
         loginPage.loginApplication(ConfigurationReader.getProperty("employee_username"),ConfigurationReader.getProperty("employee_password"));
-
         employeeDefaultPage.myOperationsDropdown.click();
         employeeDefaultPage.manageCustomers.click();
-
         createCustomerPage.createANewCustomer.click();
-        //createCustomerPage.ssn1.sendKeys("111-25-3548");
-        //createCustomerPage.searchButton.click();
-        createCustomerPage.firstname.sendKeys(Faker.instance().name().firstName());
-        createCustomerPage.lastname.sendKeys(Faker.instance().name().lastName());
-        createCustomerPage.middleInitial.sendKeys("R");
-        createCustomerPage.email.sendKeys(Faker.instance().internet().emailAddress());
-        createCustomerPage.mobilePhoneNumber.sendKeys("123-456-7890");
-        createCustomerPage.phoneNumber.sendKeys("123-456-7890");
-        createCustomerPage.zipCode.sendKeys(Faker.instance().address().zipCode());
-        createCustomerPage.address.sendKeys(Faker.instance().address().streetAddress());
-        createCustomerPage.city.sendKeys(Faker.instance().address().city());
-        createCustomerPage.ssn2.sendKeys("111-25-3548");
+        createCustomerPage.firstname.sendKeys("Testfirst");
+        createCustomerPage.lastname.sendKeys("Testlast");
+        createCustomerPage.middleInitial.sendKeys("T");
+        createCustomerPage.email.sendKeys("test@gmail.com");
+        createCustomerPage.mobilePhoneNumber.sendKeys("111-111-1111");
+        createCustomerPage.phoneNumber.sendKeys("111-111-1111");
+        createCustomerPage.zipCode.sendKeys("11111");
+        createCustomerPage.address.sendKeys("Test Address 123");
+        createCustomerPage.city.sendKeys("TestCity");
+        createCustomerPage.ssn2.sendKeys("111-11-1111");
+//        SELECTING ELEMENT FROM DROPDOWN USING SELECT OBJECT
+//        WebElement countryDropdown = Driver.getDriver().findElement(By.id("tp-customer-country"));
+//       Select select = new Select(countryDropdown);
+//       select.selectByVisibleText("USA");
 
         Select country = new Select(createCustomerPage.countryDropdown);
-        country.selectByVisibleText("TURKEY");
+        country.selectByVisibleText("USA");
 
-        createCustomerPage.state.sendKeys("Zonguldak");
+        createCustomerPage.state.sendKeys("Texas");
 
-        Select userDr = new Select(createCustomerPage.userDrop);
-        userDr.selectByIndex(3);
+        Select user = new Select(createCustomerPage.userDrop);
+        user.selectByIndex(2);
 
-        Select accountDr = new Select(createCustomerPage.accountDropdown);
-        accountDr.selectByIndex(3);
+        Select account = new Select(createCustomerPage.accountDropdown);
+        account.selectByIndex(3);
 
         createCustomerPage.zelleEnrolledCheckbox.click();
+
         createCustomerPage.saveButton.click();
 
-        ReusableMethods.getScreenshot("CustomerCreate");
+        ReusableMethods.getScreenshot("CustomerCreated");
+
 
     }
 }
